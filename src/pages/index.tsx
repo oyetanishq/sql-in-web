72a5e831dc478932bccb86d35770e617cf6f020b
Tanishq Singh <hi@tanishqsingh.com>
2024-12-01 18:03:40 +0530

    dummy data added :car:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,27 @@
 import { useState, useEffect } from "react";
 import initSqlJs, { Database } from "sql.js";
 
+import dummySqlFile from "/dummy.sqlite?url";
+import sqlWasm from "/sql-wasm.wasm?url";
+
 function App() {
 	const [db, setDb] = useState<Database | null>(null);
 
 	useEffect(() => {
-		(async () =>
-			setDb(
-				new (
-					await initSqlJs({
-						locateFile: () =>
-							"https://cdnjs.cloudflare.com/ajax/libs/sql.js/1.12.0/sql-wasm.wasm",
-					})
-				).Database()
-			))();
+		(async () => {
+			const sqlPromise = initSqlJs({ locateFile: () => sqlWasm });
+			const dataPromise = fetch(dummySqlFile).then((res) =>
+				res.arrayBuffer()
+			);
+
+			const [SQL, buf] = await Promise.all([sqlPromise, dataPromise]);
+			setDb(new SQL.Database(new Uint8Array(buf)));
+		})();
 	}, []);
+
+	useEffect(() => {
+		(window as any).db = db;
+	}, [db]);
 
 	return (
 		<div className="flex justify-center items-center h-full w-full">
