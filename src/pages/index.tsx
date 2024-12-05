4ca57cfe36e3ff7fa18ec440f9189eb601b74f96
Tanishq Singh <hi@tanishqsingh.com>
2024-12-06 03:51:38 +0530

    :sparkles: basic `code editor` with `code hightlighting`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from "react";
 import initSqlJs, { Database } from "sql.js";
 
+import Editor from "react-simple-code-editor";
+
+import { highlight, languages } from "prismjs";
+import "prismjs/components/prism-sql";
+import "prismjs/themes/prism.css";
+
 import dummySqlFile from "/dummy.sqlite?url";
 import sqlWasm from "/sql-wasm.wasm?url";
 
 function App() {
 	const [db, setDb] = useState<Database | null>(null);
+	const [code, setCode] = useState<string>("SELECT * FROM students;");
 
 	useEffect(() => {
 		(async () => {
@@ -20,12 +27,37 @@ function App() {
 	}, []);
 
 	useEffect(() => {
+		if (!db) return;
+
 		(window as any).db = db;
+		const { values } = db.exec("SELECT * FROM students;")[0];
+		console.table(values);
 	}, [db]);
 
 	return (
-		<div className="flex justify-center items-center h-full w-full">
-			{db && "aagaya"}
+		<div className="flex justify-center items-center min-h-[100dvh] w-full">
+			<nav className="h-[100dvh] w-16 flex justify-center items-center">
+				<span onClick={() => {
+					try {
+						db && alert(JSON.stringify(db.exec(code)));
+					} catch (e) {
+						alert((e as any).message)
+					}
+				}}>RUN</span>
+			</nav>
+			<main className="h-[100dvh] w-[calc(100%-4rem)] flex justify-center items-center">
+				<Editor
+					className="h-full w-full"
+					value={code}
+					onValueChange={setCode}
+					highlight={(code) => highlight(code, languages.sql, "sql")}
+					padding={15}
+					style={{
+						fontFamily: '"Fira code", "Fira Mono", monospace',
+					}}
+					tabSize={4}
+				/>
+			</main>
 		</div>
 	);
 }
