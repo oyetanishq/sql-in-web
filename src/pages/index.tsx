bae494b9ab2349bc9dc09f21816e3c6e93fb2932
Tanishq Singh <hi@tanishqsingh.com>
2024-12-06 21:07:46 +0530

    :car: editor line number, table head fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,9 @@ import { useState, useEffect } from "react";
 import initSqlJs, { Database, QueryExecResult } from "sql.js";
 
 import Editor from "react-simple-code-editor";
-
 import { highlight, languages } from "prismjs";
 import "prismjs/components/prism-sql";
-import "prismjs/themes/prism.css";
+import "prismjs/themes/prism-dark.css";
 
 import dummySqlFile from "/dummy.sqlite?url";
 import sqlWasm from "/sql-wasm.wasm?url";
@@ -45,19 +44,37 @@ function App() {
 				</span>
 			</nav>
 			<main className="h-[100dvh] w-[calc(100%-4rem)] flex justify-center items-center flex-col">
-				<Editor
-					className="h-2/3 w-full"
-					value={code}
-					onValueChange={setCode}
-					highlight={(code) => highlight(code, languages.sql, "sql")}
-					padding={15}
-					style={{
-						fontFamily: '"Fira code", "Fira Mono", monospace',
-					}}
-					tabSize={4}
-				/>
-				<div className="h-1/3 overflow-x-auto w-full px-2">
-					<table className="min-w-full divide-y-2 divide-[#ffecf4b8] bg-[#ffecf42e] text-sm border border-red-100 border-collapse">
+				<section className="h-2/3 w-full">
+					<div className="h-full w-full">
+						<Editor
+							className="h-full w-full editor"
+							value={code}
+							onValueChange={setCode}
+							highlight={(code) =>
+								highlight(code, languages.sql, "sql")
+									.split("\n")
+									.map(
+										(line, i) =>
+											`<span class='editorLineNumber'>${
+												i + 1
+											}</span>${line}`
+									)
+									.join("\n")
+							}
+							textareaId="codeArea"
+							padding={15}
+							style={{
+								fontFamily:
+									'"Fira code", "Fira Mono", monospace',
+								fontSize: 12,
+								outline: 0,
+							}}
+							tabSize={4}
+						/>
+					</div>
+				</section>
+				<div className="h-1/3 overflow-x-auto w-full px-3 pb-3">
+					<table className="min-w-full divide-y-2 divide-[#ffecf4b8] bg-[#ffecf42e] text-sm border border-red-100 border-collapse selection:bg-red-200 cursor-pointer">
 						<thead className="sticky top-0 bg-[#ffecf4a6] backdrop-blur-md">
 							<tr>
 								{data?.columns.map((value, index) => {
@@ -80,7 +97,7 @@ function App() {
 										{row.map((value, index2) => {
 											return (
 												<td
-													className="whitespace-nowrap px-4 py-2 font-medium text-gray-900"
+													className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 hover:bg-[#ffecf4]"
 													key={index2}
 												>
 													{value}
